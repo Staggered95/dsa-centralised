347e6662713eec570795e8cc0ae5ec328fdc75e5
Staggered95 <shubhamsinghup720@gmail.com>
2026-01-23 09:55:29 +0530

    dsa-centralised

---
+++ DSA/Array/1.array.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int arr[5] = {1, 2, 3};
+    cout << arr[4];
+
+    return 0;
+}
---
+++ DSA/Array/2.array.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int arr[5] = {1, 2, 3, 4,5};
+
+    for (int i=4; i>=0; i--) {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+
+    for (int i : arr) {
+        cout << 2*i << " ";
+    }
+
+    return 0;
+}
