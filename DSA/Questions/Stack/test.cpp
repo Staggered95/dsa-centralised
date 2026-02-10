2b7fac056246ffafbf1d941f6819b1fbd9047f83
Staggered95 <shubhamsinghup720@gmail.com>
2026-02-10 16:03:10 +0530

    dsa-centralised

--- DSA/Questions/Stack/test.cpp
+++ DSA/Questions/Stack/test.cpp
@@ -1,43 +1,11 @@
 class Solution {
   public:
-    int precedance(char x) {
-        if (x == '+' || x == '-') return 1;
-        if (x == '*' || x == '/') return 2;
-        if (x == '^') return 3;
-        return 0;
-    }  
-
-    string infixToPostfix(string& s) {
+    void reverseQueue(queue<int> &q) {
         // code here
-        string ans = "";
-        stack<char> st;
-
-        for (int i=0; i<s.length(); i++) {
-            char curr = s[i];
-
-            if (isalnum(curr)) {
-                ans += curr;
-            } else if (curr == '(') {
-                st.push(curr);
-            } else if (curr == ')') {
-                while (!st.empty() && st.top() != '(') {
-                    ans += st.top();
-                    st.pop();
-                }
-                st.pop();
-            } else {
-                while (!st.empty() && (precedance(st.top()) >= precedance(curr)) && (curr != '^')) {
-                    ans += st.top();
-                    st.pop();
-                }
-                st.push(curr);
-            }
-        }
-        
-        while (!st.empty()) {
-            ans += st.top();
-            st.pop();
-        }
-        return ans;
+        if (q.empty()) return;
+        int temp = q.front();
+        q.pop();
+        reverseQueue(q);
+        q.push(temp);
     }
 };
