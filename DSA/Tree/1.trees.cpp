7f42837671f3d0c15026306fb364114c9d86019e
Staggered95 <shubhamsinghup720@gmail.com>
2026-02-13 12:09:09 +0530

    dsa-centralised

---
+++ DSA/Tree/1.trees.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Tree 
+{
+    class Node
+    {
+        public:
+        Node* left;
+        int data;
+        Node* right;
+            Node(int val) : left(nullptr), data(val), right(nullptr) {}
+    }
+
+    Node* root;
+
+    Tree() : root(nullptr) {}
+
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    
+
+    return 0;
+}
