d725a55343df58d0d8af72da0bd6e26388d88a8d
Staggered95 <shubhamsinghup720@gmail.com>
2026-02-26 11:15:55 +0530

    dsa-centralised

--- DSA/Graph/test.cpp
+++ DSA/Graph/test.cpp
@@ -9,5 +9,34 @@ class Solution {
             int v = i[1];
             adj[u].push_back(v);
         }
+
+        vector<int> indegree(V, 0);
+        for (int i=0; i<V; i++) {
+            for (auto j : adj[i]) {
+                indegree[j]++;
+            }
+        }
+
+        //push all vertices with indegree 0, in queue
+        queue<int> q;
+        for (int i=0; i<V; i++) {
+            if (indegree[i] == 0) q.push(i);
+        }
+
+        vector<int> result;
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            result.push_back(node);
+            
+            for (auto neighbour : adj[node]) {
+                indegree[neighbour]--;
+                if (indegree[neighbour] == 0) {
+                    q.push(neighbour);
+                }
+            }
+        }
+
+        return result;
     }
 };
